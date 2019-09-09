aa1626303565d847d53fe582dabe9ad205f6607f
rlb <rlb@rlbs-Mac-mini.local>
2019-09-09 08:07:50 -0400

    Master Push

---
+++ db.js
@@ -0,0 +1,24 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "dssm",
+  password: "ka1ckt1999",
+  database: "dssm"
+});
+
+con.connect();
+
+  con.query("SELECT * FROM sys", function (err, result, fields) {
+	  if (err) 
+	  { 
+		 console.error(err);
+	  }
+	  else 
+	  {
+		  console.log(result)
+	  }
+  });
+
+con.end()
+
